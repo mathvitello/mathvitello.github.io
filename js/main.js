4c638247fb2a86eb37f9605f1ec795721dc7360d
mattvitello <vitellofellow@gmail.com>
2018-02-24 17:58:43 -0600

    reload

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $( document ).ready(function() {
     $("#shop-tab").click(function(){
 		console.log("ajax load")
 		var state = {};
-		$("#shop-page").load("shop.txt");
+		$("#main-page").load("shop.txt");
 		history.pushState(state,"ROY BLAIR", '/shop/');
 	});
 
