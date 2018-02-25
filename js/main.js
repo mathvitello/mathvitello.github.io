ae19c0ea5e490a373a5acd57f44381a41b5d4796
mattvitello <vitellofellow@gmail.com>
2018-02-24 18:47:47 -0600

    shop page ajax

--- js/main.js
+++ js/main.js
@@ -13,14 +13,29 @@ $(document).on("click", '#shop-tab', function(event) {
 
 $(document).on("click", '#top-nav-btn', function(event) { 
 		var state = {};
-		$("#main-page").load("home.txt");
-		history.pushState(state,"ROY BLAIR", '/shop/');
+		$("#main-page").load("../home.txt");
+		history.pushState(state,"ROY BLAIR", '');
+});
+
+$(document).on("click", '#shirt-prev', function(event) { 
+		var state = {};
+		$("#main-page").load("../shirt.txt");
+		history.pushState(state,"ROY BLAIR", '/shop/shirt.html');
+});
+
+$(document).on("click", '#longsleeve-prev', function(event) { 
+		var state = {};
+		$("#main-page").load("../longsleeve.txt");
+		history.pushState(state,"ROY BLAIR", '/shop/longsleeve.html');
+});
+
+$(document).on("click", '#hoodie-prev', function(event) { 
+		var state = {};
+		$("#main-page").load("../hoodie.txt");
+		history.pushState(state,"ROY BLAIR", '/shop/hoodie.html');
 });
 
 
-
 window.onpopstate = function(event) {
-    if(event && event.state) {
-        location.reload();
-    }
+	location.reload();
 }
