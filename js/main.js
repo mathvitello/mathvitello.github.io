a77a87828fb8c8fe8888c55b944a61f5bab2b4f2
mattvitello <vitellofellow@gmail.com>
2018-02-24 18:15:51 -0600

    made better

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $( document ).ready(function() {
 	var state = {};
 	$("#main-page").load("home.txt");
-});
 
-$("#shop-tab").click(function(){
-	console.log("ajax load")
-	var state = {};
-	$("#main-page").replaceWith("shop.txt");
-	history.pushState(state,"ROY BLAIR", '/shop/');
+	$("#shop-tab").click(function(){
+		console.log("ajax load")
+		var state = {};
+		$("#main-page").replaceWith("shop.txt");
+		history.pushState(state,"ROY BLAIR", '/shop/');
+	});
 });
