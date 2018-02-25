eb001fb6ecf226fa66976b6a6b22cae91d200bbb
mattvitello <vitellofellow@gmail.com>
2018-02-24 18:21:22 -0600

    ajax loaf

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $( document ).ready(function() {
 	var state = {};
 	$("#main-page").load("home.txt");
+});
 
-	$("#shop-tab").click(function(){
-		console.log("ajax load")
+$(document).on("click", '#shop-tab', function(event) { 
+		console.log("ajax load");
 		var state = {};
 		$("#main-page").replaceWith("shop.txt");
 		history.pushState(state,"ROY BLAIR", '/shop/');
-	});
 });
