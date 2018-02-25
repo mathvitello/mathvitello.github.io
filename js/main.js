003c194dcabc2ba091d0716d37b36e9eb1971c8e
mattvitello <vitellofellow@gmail.com>
2018-02-24 18:02:48 -0600

    changed to html

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 $( document ).ready(function() {
 	var state = {};
-	$("#main-page").load("home.txt");
+	$("#main-page").html("home.txt");
 	history.pushState(state,"ROY BLAIR", '');
 
     $("#shop-tab").click(function(){
+    	$("#main-page").hide();
+
 		console.log("ajax load")
 		var state = {};
-		$("#main-page").load("shop.txt");
+		$("#main-page").html("shop.txt");
 		history.pushState(state,"ROY BLAIR", '/shop/');
 	});
 
